45af43b9d87d36a14a24e85529263178a2672c45
brootroot <seanlbray@gmail.com>
2017-11-27 12:19:51 -0500

    Trigger build.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Landing from './components/landing';
 import Registration from './components/registration';
@@ -12,17 +12,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <Route path="/registration" component={Registration} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/resources" component={Resources} />
-          <Route exact path="/log" component={WorkoutLog} />
-          <Footer />
-        </div>
-      </Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route path="/registration" component={Registration} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/resources" component={Resources} />
+        <Route exact path="/log" component={WorkoutLog} />
+        <Footer />
+      </div>
     );
   }
 }
